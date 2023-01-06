f4e02c1dd6d71cf3f10472426a9d81df4ad79758
raashidbaba <raashidbaba849@gmail.com>
2023-01-07 00:29:58 +0530

    checking on git

---
+++ app/src/main/java/com/example/coroutines/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.example.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        Log.d("activity","run a log")
+    }
+}
