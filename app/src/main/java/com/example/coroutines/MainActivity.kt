5239078cd0cb7aeb1b5183ad450a405aa467101f
EnnBi <nadym.baba@gmail.com>
2023-01-06 23:53:44 +0530

    understanding coroutine jobs.

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -10,24 +10,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-val TAG = "my activity"
-      GlobalScope.launch(Dispatchers.IO) {
-          Log.d(TAG,"this is my first {${Thread.currentThread().name}}")
-          val answer = myNetworkCall()
-          withContext(Dispatchers.Main){
-              Log.d(TAG,"this is my second {${Thread.currentThread().name}}")
-              val myText = findViewById<TextView>(R.id.textview)
-              myText.text = answer
-          }
-      }
+        val TAG = "my activity"
+           /*  Log.d(TAG, "this is my second {${Thread.currentThead().name}}")
+                val myText = findViewById<TextView>(R.id.textview)*/
+              val myjob =   GlobalScope.launch(Dispatchers.Default) {
+                  repeat(5){
+                      Log.d(TAG,"is still working")
+
+
+                   /* val answer = myNetworkCall()
+                    withContext(Dispatchers.Main) {
+                    myText.text = answer*/
+            }
+        }
+        runBlocking {
+            myjob.join()
+
+        }
 
     }
-   suspend fun myNetworkCall():String{
+
+    suspend fun myNetworkCall(): String {
         delay(3000L)
         return "this is my call"
     }
-   /* suspend fun myNetworkCall2():String{
-        delay(3000L)
-        return "this is my call 2"
-    }*/
+    /* suspend fun myNetworkCall2():String{
+         delay(3000L)
+         return "this is my call 2"
+     }*/
 }
