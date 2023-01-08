cd0b3b77032294ff8c14abaa1ea2c4bf15edbf73
raashidbaba <raashidbaba849@gmail.com>
2023-01-08 22:29:45 +0530

    using async for network call.

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,20 +14,22 @@ class MainActivity : AppCompatActivity() {
         val TAG = "my activity"
            /*  Log.d(TAG, "this is my second {${Thread.currentThead().name}}")
                 val myText = findViewById<TextView>(R.id.textview)*/
-              val myjob =   GlobalScope.launch(Dispatchers.Default) {
-                  repeat(5){
-                      Log.d(TAG,"is still working")
-
+                 GlobalScope.launch(Dispatchers.IO) {
+                 val time = measureTimeMillis {
+                     val answer1 = async { myNetworkCall() }
+                     val answer2 = async { myNetworkCall2() }
+                     Log.d(TAG,"answer 1 ${answer1.onAwait}")
+                     Log.d(TAG,"answer 2 ${answer2.onAwait}")
+                 }
+                     Log.d(TAG,"request took $time")
 
                    /* val answer = myNetworkCall()
                     withContext(Dispatchers.Main) {
                     myText.text = answer*/
             }
-        }
-        runBlocking {
-            myjob.join()
 
         }
+
 
     }
 
@@ -34,8 +37,7 @@ class MainActivity : AppCompatActivity() {
         delay(3000L)
         return "this is my call"
     }
-    /* suspend fun myNetworkCall2():String{
+     suspend fun myNetworkCall2():String{
          delay(3000L)
          return "this is my call 2"
-     }*/
-}
+     }
