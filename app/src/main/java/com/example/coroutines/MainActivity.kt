21c422536687fc51c60539964613b2e1c553e74b
raashidbaba <raashidbaba849@gmail.com>
2023-01-10 23:46:27 +0530

    adv of lifeCycleScope.

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.coroutines
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
@@ -11,33 +14,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val TAG = "my activity"
-           /*  Log.d(TAG, "this is my second {${Thread.currentThead().name}}")
-                val myText = findViewById<TextView>(R.id.textview)*/
-                 GlobalScope.launch(Dispatchers.IO) {
-                 val time = measureTimeMillis {
-                     val answer1 = async { myNetworkCall() }
-                     val answer2 = async { myNetworkCall2() }
-                     Log.d(TAG,"answer 1 ${answer1.onAwait}")
-                     Log.d(TAG,"answer 2 ${answer2.onAwait}")
-                 }
-                     Log.d(TAG,"request took $time")
 
-                   /* val answer = myNetworkCall()
-                    withContext(Dispatchers.Main) {
-                    myText.text = answer*/
+
+        val mybutton = findViewById<Button>(R.id.btn1)
+        mybutton.setOnClickListener {
+lifecycleScope.launch {
+    delay(1000L)
+    while (true){
+        Log.d("activity","still running")
+    }
+}
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity,MainActivity2::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
             }
-
         }
-
-
     }
-
-    suspend fun myNetworkCall(): String {
-        delay(3000L)
-        return "this is my call"
-    }
-     suspend fun myNetworkCall2():String{
-         delay(3000L)
-         return "this is my call 2"
-     }
+}
